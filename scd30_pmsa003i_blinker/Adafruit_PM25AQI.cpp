d0b4b730594ae638c39ac9264e7859efb5eb53f1
Ivan Jiang <jiangyi_1985@hotmail.com>
2022-07-24 01:24:12 +0800

    scd30+pmsa003i+blinker

---
+++ scd30_pmsa003i_blinker/Adafruit_PM25AQI.cpp
@@ -0,0 +1,320 @@
+/*!
+ * @file Adafruit_PM25AQI.cpp
+ *
+ * @mainpage Adafruit PM2.5 air quality sensor driver
+ *
+ * @section intro_sec Introduction
+ *
+ * This is the documentation for Adafruit's PM2.5 AQI driver for the
+ * Arduino platform.  It is designed specifically to work with the
+ * Adafruit PM2.5 Air quality sensors: http://www.adafruit.com/products/4632
+ *
+ * These sensors use I2C or UART to communicate.
+ *
+ * Adafruit invests time and resources providing this open source code,
+ * please support Adafruit and open-source hardware by purchasing
+ * products from Adafruit!
+ *
+ *
+ * @section author Author
+ * Written by Ladyada for Adafruit Industries.
+ *
+ * @section license License
+ * BSD license, all text here must be included in any redistribution.
+ *
+ */
+
+#include <math.h>
+#include "Adafruit_PM25AQI.h"
+
+/*!
+ *  @brief  Instantiates a new PM25AQI class
+ */
+Adafruit_PM25AQI::Adafruit_PM25AQI() {}
+
+/*!
+ *  @brief  Setups the hardware and detects a valid PMSA003I. Initializes I2C.
+ *  @param  theWire
+ *          Optional pointer to I2C interface, otherwise use Wire
+ *  @return True if PMSA003I found on I2C, False if something went wrong!
+ */
+bool Adafruit_PM25AQI::begin_I2C(TwoWire *theWire) {
+  if (!i2c_dev) {
+    i2c_dev = new Adafruit_I2CDevice(PMSA003I_I2CADDR_DEFAULT, theWire);
+  }
+
+  if (!i2c_dev->begin()) {
+    return false;
+  }
+
+  return true;
+}
+
+/*!
+ *  @brief  Setups the hardware and detects a valid UART PM2.5
+ *  @param  theSerial
+ *          Pointer to Stream (HardwareSerial/SoftwareSerial) interface
+ *  @return True
+ */
+bool Adafruit_PM25AQI::begin_UART(Stream *theSerial) {
+  serial_dev = theSerial;
+
+  return true;
+}
+
+/*!
+ *  @brief  Setups the hardware and detects a valid UART PM2.5
+ *  @param  data
+ *          Pointer to PM25_AQI_Data that will be filled by read()ing
+ *  @return True on successful read, false if timed out or bad data
+ */
+bool Adafruit_PM25AQI::read(PM25_AQI_Data *data) {
+  uint8_t buffer[32];
+  uint16_t sum = 0;
+
+  if (!data) {
+    return false;
+  }
+
+  if (i2c_dev) { // ok using i2c?
+    if (!i2c_dev->read(buffer, 32)) {
+      return false;
+    }
+  } else if (serial_dev) { // ok using uart
+    if (!serial_dev->available()) {
+      return false;
+    }
+    int skipped = 0;
+    while ((skipped < 32) && (serial_dev->peek() != 0x42)) {
+      serial_dev->read();
+      skipped++;
+      if (!serial_dev->available()) {
+        return false;
+      }
+    }
+    if (serial_dev->peek() != 0x42) {
+      serial_dev->read();
+      return false;
+    }
+    // Now read all 32 bytes
+    if (serial_dev->available() < 32) {
+      return false;
+    }
+    serial_dev->readBytes(buffer, 32);
+  } else {
+    return false;
+  }
+
+  memcpy((void *)data->raw, (void *)buffer, 32);
+
+  // Check that start byte is correct!
+  if (buffer[0] != 0x42 || buffer[1] != 0x4d) {
+    data->startbyte_fail = 1;
+    return false;
+  }
+  else{data->startbyte_fail = 0;}
+
+  // get checksum ready
+  for (uint8_t i = 0; i < 30; i++) {
+    sum += buffer[i];
+  }
+
+  // The data comes in big endian (MSB first), this solves it so it works on all platforms
+  uint16_t buffer_u16[15];
+  for (uint8_t i = 0; i < 15; i++) {
+    buffer_u16[i] = buffer[2 + i * 2 + 1];
+    buffer_u16[i] += (buffer[2 + i * 2] << 8);
+  }
+
+  // put it into a nice struct :)
+  memcpy((void *)data, (void *)buffer_u16, 30);
+
+  data->datasum = sum;
+  if (sum != data->checksum) {
+    data->checksum_fail = 1;
+    return false;
+  }
+  else{data->checksum_fail = 0;}
+
+  data->version=buffer[28];
+  data->error_code=buffer[29];
+
+  //convert concentration to AQI
+  data->aqi_pm25_us=pm25_aqi_us(data->pm25_env);
+  data->aqi_pm25_china=pm25_aqi_china(data->pm25_env);
+  data->aqi_pm100_us=pm100_aqi_us(data->pm100_env);
+  data->aqi_pm100_china=pm100_aqi_china(data->pm100_env);
+
+  // success!
+  return true;
+}
+
+uint16_t Adafruit_PM25AQI::pm25_aqi_us(float concentration) {
+  float c;
+  float AQI;
+  c=(floor(10*concentration))/10;
+  if(c<0)
+    AQI=0;
+  else if (c>=0 && c<12.1f)
+  {
+    AQI=linear(50,0,12,0,c);
+  }
+  else if (c>=12.1f && c<35.5f)
+  {
+    AQI=linear(100,51,35.4f,12.1f,c);
+  }
+  else if (c>=35.5f && c<55.5f)
+  {
+    AQI=linear(150,101,55.4f,35.5f,c);
+  }
+  else if (c>=55.5f && c<150.5f)
+  {
+    AQI=linear(200,151,150.4f,55.5f,c);
+  }
+  else if (c>=150.5f && c<250.5f)
+  {
+    AQI=linear(300,201,250.4f,150.5f,c);
+  }
+  else if (c>=250.5f && c<350.5f)
+  {
+    AQI=linear(400,301,350.4f,250.5f,c);
+  }
+  else if (c>=350.5f && c<500.5f)
+  {
+    AQI=linear(500,401,500.4f,350.5f,c);
+  }
+  else
+  {
+    AQI=99999;//
+  }
+  return round(AQI);
+}
+
+uint16_t Adafruit_PM25AQI::pm100_aqi_us(float concentration) {
+  float c;
+  float AQI;
+  c=concentration;
+  if(c<0)
+    AQI=0;
+  else if (c<55)
+  {
+    AQI=linear(50,0,55,0,c);
+  }
+  else if (c<155)
+  {
+    AQI=linear(100,51,155,55,c);
+  }
+  else if (c<255)
+  {
+    AQI=linear(150,101,255,155,c);
+  }
+  else if (c<355)
+  {
+    AQI=linear(200,151,355,255,c);
+  }
+  else if (c<425)
+  {
+    AQI=linear(300,201,425,355,c);
+  }
+  else if (c<505)
+  {
+    AQI=linear(400,301,505,425,c);
+  }
+  else if (c<605)
+  {
+    AQI=linear(500,401,605,505,c);
+  }
+  else
+  {
+    AQI=99999;//
+  }
+  return round(AQI);
+}
+
+uint16_t Adafruit_PM25AQI::pm25_aqi_china(float concentration) {
+  float c;
+  float AQI;
+  c=concentration;
+  if(c<0)
+    AQI=0;
+  else if (c<=35)
+  {
+    AQI=linear(50,0,35,0,c);
+  }
+  else if (c<=75)
+  {
+    AQI=linear(100,51,75,35,c);
+  }
+  else if (c<=115)
+  {
+    AQI=linear(150,101,115,75,c);
+  }
+  else if (c<=150)
+  {
+    AQI=linear(200,151,150,115,c);
+  }
+  else if (c<=250)
+  {
+    AQI=linear(300,201,250,150,c);
+  }
+  else if (c<=350)
+  {
+    AQI=linear(400,301,350,250,c);
+  }
+  else if (c<=500)
+  {
+    AQI=linear(500,401,500,350,c);
+  }
+  else
+  {
+    AQI=99999;//
+  }
+  return round(AQI);
+}
+
+uint16_t Adafruit_PM25AQI::pm100_aqi_china(float concentration) {
+  float c;
+  float AQI;
+  c=concentration;
+  if(c<0)
+    AQI=0;
+  else if (c<=50)
+  {
+    AQI=linear(50,0,50,0,c);
+  }
+  else if (c<=150)
+  {
+    AQI=linear(100,51,150,50,c);
+  }
+  else if (c<=250)
+  {
+    AQI=linear(150,101,250,150,c);
+  }
+  else if (c<=350)
+  {
+    AQI=linear(200,151,350,250,c);
+  }
+  else if (c<=420)
+  {
+    AQI=linear(300,201,420,350,c);
+  }
+  else if (c<=500)
+  {
+    AQI=linear(400,301,500,420,c);
+  }
+  else if (c<=600)
+  {
+    AQI=linear(500,401,600,500,c);
+  }
+  else
+  {
+    AQI=99999;//
+  }
+  return round(AQI);
+}
+
+float Adafruit_PM25AQI::linear(uint16_t aqi_high, uint16_t aqi_low, float conc_high, float conc_low, float concentration) {
+  float f;
+  f = ((concentration-conc_low)/(conc_high-conc_low)) * (aqi_high-aqi_low) + aqi_low;
+  return f;
+}
---
+++ scd30_pmsa003i_blinker/scd30_pmsa003i_blinker.ino
@@ -0,0 +1,189 @@
+#include "Adafruit_PM25AQI.h"
+#include <Wire.h>
+#include "SparkFun_SCD30_Arduino_Library.h"
+#define BLINKER_WIFI
+#include <Blinker.h>
+
+SCD30 airSensor;
+Adafruit_PM25AQI aqi = Adafruit_PM25AQI();
+
+char auth[] = "xxxxxxxxxxxx"; //上一步中在app中获取到的Secret Key
+char ssid[] = "xxxxxxxx"; //您的WiFi热点名称
+char pswd[] = "xxxxxxxx"; //您的WiFi密码
+
+BlinkerNumber NumberTemp("num-temp");//defines a blinker number
+BlinkerNumber NumberRh("num-rh");
+BlinkerNumber NumberCo2("num-co2");
+BlinkerNumber NumberPart03("num-part03");
+BlinkerNumber NumberPart05("num-part05");
+BlinkerNumber NumberPart10("num-part10");
+BlinkerNumber NumberPart25("num-part25");
+BlinkerNumber NumberPart50("num-part50");
+BlinkerNumber NumberPart100("num-part100");
+BlinkerNumber NumberPm25ConcEnv("num-pm25conc-env");
+BlinkerNumber NumberPm100ConcEnv("num-pm100conc-env");
+BlinkerNumber NumberPm25aqiUS("num-pm25aqi-us");
+BlinkerNumber NumberPm100aqiUS("num-pm100aqi-us");
+BlinkerNumber NumberPm25aqiCN("num-pm25aqi-cn");
+BlinkerNumber NumberPm100aqiCN("num-pm100aqi-cn");
+
+void setup() {
+  Serial.begin(115200);
+  while(Serial == false){};
+  Wire.begin();
+
+      delay(500);
+  while (airSensor.begin() == false)
+  {
+      Serial.println("CO2 sensor not detected. retrying...");
+      delay(500);
+  }
+  
+  delay(500);
+  while (! aqi.begin_I2C()) {      // connect to the sensor over I2C
+    Serial.println("PM 2.5 sensor not detected. retrying...");
+    delay(500);
+  }
+
+    Blinker.begin(auth, ssid, pswd);//start blinker wifi mode
+    Blinker.attachHeartbeat(heartbeat);//app定时向设备发送心跳包, 设备收到心跳包后会返回设备当前状态，如果用户有自定义状态需要在收到心跳包时返回, 可调用该函数
+
+    //Blinker.attachDataStorage(func, time, times); func, 对应的回调函数
+//    time, 对应数据采集时间-默认60 s, 最少60 s
+//    times, 对应采集次数后上传到云端-默认2次, 最多不超过4次 
+    Blinker.attachDataStorage(dataStorage);//关联回调函数，开启历史数据存储功能
+}
+
+float RH,T;
+int CO2,PART03,PART05,PART10,PART25,PART50,PART100,PM25CONCenv,PM100CONCenv,PM25AQIus,PM100AQIus,PM25AQIchina,PM100AQIchina;
+int seq=0;
+
+void loop() {
+  if (airSensor.dataAvailable())
+    {
+        int co2 = airSensor.getCO2();
+        float t = airSensor.getTemperature();
+        float rh = airSensor.getHumidity();
+        
+        if(co2==0 || t==0 || rh==0){
+          Serial.println("scd30 0 data...");
+        } else {
+          CO2 = co2;
+          T = t;
+          RH = rh;
+          
+          Serial.println(F("***************************************"));
+          Serial.print("co2(ppm):");        Serial.print(CO2);
+          Serial.print("\ttemp(C):");        Serial.print(T);
+          Serial.print("\thumidity(%):");        Serial.println(RH);
+          Serial.println(F("***************************************"));
+        }
+    }
+    else
+        Serial.println("scd30 no data...");
+  
+  PM25_AQI_Data data;  
+  if (aqi.read(&data)) {
+    PART03=data.particles_03um;
+    PART05=data.particles_05um;
+    PART10=data.particles_10um;
+    PART25=data.particles_25um;
+    PART50=data.particles_50um;
+    PART100=data.particles_100um;
+    PM25CONCenv=data.pm25_env;
+    PM100CONCenv=data.pm100_env;
+    PM25AQIus=data.aqi_pm25_us;
+    PM100AQIus=data.aqi_pm100_us;
+    PM25AQIchina=data.aqi_pm25_china;
+    PM100AQIchina=data.aqi_pm100_china;
+    Serial.println(F("---------------------------------------"));
+    Serial.print(F("PM1.0: ")); Serial.print(data.pm10_env); Serial.print("μg/m3");//PM1.0浓度（大气环境下）单位μg/m3
+    Serial.print(F("\t\tPM2.5: ")); Serial.print(data.pm25_env); Serial.print("μg/m3");//PM2.5浓度（大气环境下）单位μg/m3
+    Serial.print(F("\t\tPM10: ")); Serial.print(data.pm100_env); Serial.println("μg/m3");//PM10浓度 （大气环境下）单位μg/m3
+    Serial.print(F("PM2.5 AQI US: ")); Serial.print(data.aqi_pm25_us);
+    Serial.print(F("\tPM2.5 AQI China: ")); Serial.print(data.aqi_pm25_china);
+    Serial.print(F("\tPM10  AQI US: ")); Serial.print(data.aqi_pm100_us);
+    Serial.print(F("\tPM10  AQI China: ")); Serial.println(data.aqi_pm100_china);
+    Serial.print(F("Particles/0.1L:\t>0.3um:")); Serial.print(data.particles_03um);    Serial.print(F("\t>0.5um:")); Serial.print(data.particles_05um);
+    Serial.print(F("\t>1.0um:")); Serial.print(data.particles_10um);    Serial.print(F("\t>2.5um:")); Serial.print(data.particles_25um);
+    Serial.print(F("\t>5.0um:")); Serial.print(data.particles_50um);    Serial.print(F("\t>10um:")); Serial.println(data.particles_100um);
+    Serial.print(F("0.3~0.5μm:")); Serial.println(data.particles_03um-data.particles_05um);
+    Serial.println(F("---------------------------------------"));
+  } else {
+    Serial.println("pmsa003i no data...");
+    //printInfo(data);
+  }
+
+    Serial.println();
+  delay(1000);
+
+  Blinker.run();//此函数需要频繁调用以保持设备间连接及处理收到的数据, 建议放在 loop() 函数中
+}
+
+void dataStorage(){//在回调函数中，设定要存储的键名和值 //键值可以跟blinker number一样或者不一样
+  //缓存数据最大为 256 字节
+  //最多可以同时缓存 6 个不同的key 
+  
+  //免费用户只有5个存储key，用完就不能再存了，可以通过菜单"开发者"进去，删除现有的key
+
+   Blinker.dataStorage("num-co2", CO2);
+//   Blinker.dataStorage("num-temp", T);
+//   Blinker.dataStorage("num-rh", RH);
+
+//   Blinker.dataStorage("num-pm25conc-env", PM25CONCenv);
+//   Blinker.dataStorage("num-pm100conc-env", PM100CONCenv);
+   Blinker.dataStorage("num-pm25aqi-us", PM25AQIus);
+   Blinker.dataStorage("num-pm100aqi-us", PM100AQIus);
+//   Blinker.dataStorage("num-pm25aqi-cn", PM25AQIchina);
+//   Blinker.dataStorage("num-pm100aqi-cn", PM100AQIchina);
+   
+//   Blinker.dataStorage("num-part03", PART03);
+//   Blinker.dataStorage("num-part05", PART05);
+//   Blinker.dataStorage("num-part10", PART10);
+//   Blinker.dataStorage("num-part25", PART25);
+//   Blinker.dataStorage("num-part50", PART50);
+//   Blinker.dataStorage("num-part100", PART100);
+
+//   Blinker.dataStorage("num-part03-25", PART03-PART25);
+   Blinker.dataStorage("num-part03-05", PART03-PART05);
+   Blinker.dataStorage("num-part05-25", PART05-PART25);
+   Blinker.dataStorage("num-part25-100", PART25-PART100);
+   Blinker.dataStorage("num-part100", PART100);
+}
+
+void heartbeat(){
+    NumberTemp.print(T);//send blinker number data to app
+    NumberRh.print(RH);
+    NumberCo2.print(CO2);
+    NumberPart03.print(PART03);
+    NumberPart05.print(PART05);
+    NumberPart10.print(PART10);
+    NumberPart25.print(PART25);
+    NumberPart50.print(PART50);
+    NumberPart100.print(PART100);
+    NumberPm25ConcEnv.print(PM25CONCenv);
+    NumberPm100ConcEnv.print(PM100CONCenv);
+    NumberPm25aqiUS.print(PM25AQIus);
+    NumberPm100aqiUS.print(PM100AQIus);
+    NumberPm25aqiCN.print(PM25AQIchina);
+    NumberPm100aqiCN.print(PM100AQIchina);
+}
+
+void printInfo(PM25_AQI_Data data)
+{
+  Serial.print(F("startbyte_fail: ")); Serial.println(data.startbyte_fail);
+  Serial.print(F("checksum_fail: ")); Serial.println(data.checksum_fail);
+  Serial.print(F("framelen: ")); Serial.println(data.framelen);
+  Serial.print(F("version: ")); Serial.println(data.version);
+  Serial.print(F("error_code: ")); Serial.println(data.error_code);
+  Serial.print(F("checksum: ")); Serial.println(data.checksum);
+  Serial.print(F("datasum: ")); Serial.println(data.datasum);
+  Serial.println(F("raw data: "));
+  for(int i = 0; i < sizeof(data.raw); i++)
+  {
+    Serial.print(data.raw[i]);
+    Serial.print("\t");
+    if((i+1)%2==0)
+      Serial.println();
+  }
+}
