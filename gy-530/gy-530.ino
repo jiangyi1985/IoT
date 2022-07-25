a9b4dce80612887d11c65ec253fae3e852af83f9
Ivan Jiang <jiangyi_1985@hotmail.com>
2022-07-25 13:20:38 +0800

    gy-530 vl53l0x

---
+++ gy-530/gy-530.ino
@@ -0,0 +1,42 @@
+/* 
+ * SGP41    NodeMcu-32S   NodeMcu-8266
+ * SDA      GPIO21        GPIO04
+ * SCL      GPIO22        GPIO05
+ * +        3.3V          3.3V
+ */
+#include "Adafruit_VL53L0X.h"
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+void setup() {
+  Serial.begin(115200);
+
+  // wait until serial port opens for native USB devices
+  while (! Serial) {
+    delay(1);
+  }
+  
+  Serial.println("Adafruit VL53L0X test");
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+  // power 
+  Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
+}
+
+
+void loop() {
+  VL53L0X_RangingMeasurementData_t measure;
+    
+  Serial.print("Reading a measurement... ");
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+  } else {
+    Serial.println(" out of range ");
+  }
+    
+  delay(100);
+}
